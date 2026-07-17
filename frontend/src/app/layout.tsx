c727678fe918216af24dbfa1d332dff05d28d539
GhilhermeAlves <ghilherme007@hotmail.com>
2026-07-17 03:24:21 -0300

    configuração do docker-compose na vps hostinger

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
+    <html lang="pt-BR">
+      <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
